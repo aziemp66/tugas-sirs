f0826cc61bd22f0c1c4ff6fb725f58e5d348c928
Azie Melza Pratama <aziemp66@gmail.com>
2024-05-03 02:48:39 +0700

    fix : Adding Pointers to Slice in Repository Contract

--- internal/modules/ecg/repository/postgres/init.go
+++ internal/modules/ecg/repository/postgres/init.go
@@ -1,10 +1,12 @@
 package ecg_repository_postgres
 
 import (
+	ecg_repository "tugas-sirs/internal/modules/ecg/repository"
+
 	"github.com/jmoiron/sqlx"
 )
 
-func NewEcgRepositoryPostgres(db *sqlx.DB) *ecgRepositoryPostgres {
+func NewEcgRepositoryPostgres(db *sqlx.DB) ecg_repository.EcgRepository {
 	return &ecgRepositoryPostgres{
 		db: db,
 	}
--- internal/modules/ecg/repository/type.go
+++ internal/modules/ecg/repository/type.go
@@ -8,7 +8,7 @@ import (
 type EcgRepository interface {
 	CreateEcg(ctx context.Context, name string, age int, sex bool, heartFailure ecg_entity.HeartFailureClass) (id string, err error)
 	FindEcgByID(ctx context.Context, id string) (*ecg_entity.Ecg, error)
-	FindAllEcg(ctx context.Context) ([]*ecg_entity.Ecg, error)
+	FindAllEcg(ctx context.Context) (*[]*ecg_entity.Ecg, error)
 	UpdateEcgByID(ctx context.Context, id string, name string, age int, sex bool, heartFailure ecg_entity.HeartFailureClass) error
 	DeleteEcgByID(ctx context.Context, id string) error
 }
